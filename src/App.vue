087c2bd86a1c85c792670b2b1c21ec43de9c3ac7
bakhodir <bakhodir.sodikov2002@mail.ru>
2023-11-17 11:46:19 +0500

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <button class="btn form__submit-btn"
+  v-if="!formShow"
+  @click="formShow = true"
+  >Добавить</button>
+  <div class="container-form" v-show="formShow">
+    <div class="form" >
+      <span class="form__close-container">
+        <button class="btn delete-btn" 
+        @click="formShow = false">
+          X
+        </button>
+      </span>
+      <h2 :class="{'add' : toAdd, 'change': toChange}">{{ formTitle }}</h2>
+      <form action="" method="get" class="form-example">
+        
+        
+          <input type="text"
+          placeholder="Имя"
+          v-model="name" required />
+
+          <input type="text" 
+          placeholder="Фамилия"
+          v-model="lastName" required />
+
+          <input type="number"
+          placeholder="Имя"
+          v-model="experience" required />
+
+          <input type="number"
+          placeholder="Возраст"
+          v-model="age" required />
+
+          <input type="text"
+          placeholder="Адресс"
+          v-model="adress" required />
+
+        <div class="form-el">
+          <button 
+            class="btn form__submit-btn"
+            v-if="toAdd"
+            @click="getData"> {{ formSubmit }} 
+          </button>
+          <button 
+            class="btn form__change-btn"
+            v-if="toChange"
+            @click="saveChanged"> {{ formSubmit }} 
+          </button>
+        </div>
+      </form>
+    </div>
+  </div> 
+  <div class="list">
+    <h2 class=""> Список работников</h2>
+    <table class="workers-table">
+      <thead>
+        <tr>
+          <th>№</th>
+          <th>Имя</th>
+          <th>Фамилия</th>
+          <th>Стаж</th>
+          <th>Возраст</th>
+          <th>Адресс</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody v-if="list">
+        <tr v-for="(person, idx) in list"
+        :key="idx">
+          <td>{{ idx + 1 }}</td>
+          <td>{{ person.name }}</td>
+          <td>{{ person.lastName }}</td>
+          <td>{{ person.experience }}</td>
+          <td>{{ person.age }}</td>
+          <td>{{ person.adress }}</td>
+          <td >
+            <button class="btn"
+            @click="toChangeWorker(idx)"
+            >Изменить</button> 
+            <button class="btn delete-btn"
+            @click="deleteWorker(idx)"
+            >Удалить</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style src="./app.css">
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
+<script src="./main.js"></script>
+
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,97 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+export default{
+  data(){
+    return{
+      formSubmit: 'Добавить',
+      formTitle: 'Заполнить форму',
+      formShow: false,
+      toAdd: true,
+      toChange: false,
+      name: '',
+      lastName: '',
+      experience: '',
+      age: '',
+      adress: '',
+      list: [],
+      changeId: 0,
+    }
+  },
+  created(){
+    this.getList()
+    
+  },
+  methods: {
+    updateLocal(){
+      localStorage.removeItem('worker-list');
+        let workerList = JSON.stringify(this.list)
+        localStorage.setItem('worker-list', workerList)
+    },
+    clearForm(){
+        this.name = '';
+        this.lastName = '';
+        this.experience = '';
+        this.age = '';
+        this.adress = '';
+    },
+    getData(){
+      if(this.name && this.lastName && this.experience && this.age && this.adress){
+        const newWorker = {
+          name: this.name,
+          lastName: this.lastName,
+          experience: this.experience,
+          age: this.age,
+          adress: this.adress
+        }
+        this.list.push(newWorker);
+        this.clearForm();
+        this.updateLocal();
+        this.formShow = false
+      }
+    },
+    getList(){
+      const workerList = JSON.parse(localStorage.getItem('worker-list'));
+      this.list = workerList
+    },
+
+    deleteWorker(idx){
+      this.list.splice(idx, 1);
+      this.updateLocal();
+    },
+
+    toChangeWorker(idx){
+      this.formShow = true;
+      const changeWorker = this.list[idx];
+      this.name = changeWorker.name;
+      this.lastName = changeWorker.lastName;
+      this.experience = changeWorker.experience;
+      this.age = changeWorker.age;
+      this.adress = changeWorker.adress;
+      this.toAdd = false;
+      this.toChange = true;
+      this.formTitle = 'Изменить содержимое';
+      this.formSubmit = 'Изменить';
+      this.changeId = idx
+      
+    },
+    saveChanged(){
+      if(this.name && this.lastName && this.experience && this.age && this.adress){
+        const changedWorker = {
+          name: this.name,
+          lastName: this.lastName,
+          experience: this.experience,
+          age: this.age,
+          adress: this.adress
+        }
+      this.list.splice(this.changeId, 1, changedWorker);
+      this.clearForm();
+      this.toAdd = true;
+      this.toChange = false;
+      this.formShow = false
+      this.updateLocal();
+      }
+    }
+  }
+}
 createApp(App).mount('#app')
